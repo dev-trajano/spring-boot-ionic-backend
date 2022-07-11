4564b9a4c88395fd4cf9c569333ade4fc8451a10
Joanderson Trajano <trajanojoanderson@gmail.com>
2022-07-11 16:59:43 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/trajano/cursomc/services/CategoriaService.java
+++ src/main/java/com/trajano/cursomc/services/CategoriaService.java
@@ -7,16 +7,18 @@ import org.springframework.stereotype.Service;
 
 import com.trajano.cursomc.domain.Categoria;
 import com.trajano.cursomc.repositories.CategoriaRepository;
+import com.trajano.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
-	                                 
+
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 		}
 
 }
