e39085acdbe97b1bed2b1b61b93fdf2276f5b3c4
Joanderson Trajano <trajanojoanderson@gmail.com>
2022-07-13 23:40:52 -0300

    Cliente, TipoCliente, Telefones e enderecos

---
+++ src/main/java/com/trajano/cursomc/resources/ClienteResource.java
@@ -0,0 +1,29 @@
+package com.trajano.cursomc.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.trajano.cursomc.domain.Cliente;
+import com.trajano.cursomc.services.ClienteService;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class ClienteResource {
+	
+	@Autowired
+	private ClienteService service;
+
+	@RequestMapping(value="/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		
+		Cliente obj = service.find(id);
+		return ResponseEntity.ok().body(obj);
+		
+		
+	}
+
+}
