3f9476537edf4e3e8bd9768305a65e893f0bdb8d
Joanderson Trajano <trajanojoanderson@gmail.com>
2022-09-02 20:46:00 -0300

    Personalizando o retorno da validação

--- src/main/java/com/trajano/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/trajano/cursomc/dto/CategoriaDTO.java
@@ -12,7 +12,7 @@ public class CategoriaDTO implements Serializable {
 
 	private Integer id;
 
-	@NotEmpty (message = "Prenchimento irrigation")
+	@NotEmpty(message = "Preenchimento obrigatório")
 	@Length(min = 5, max = 80, message = "O tamanho deve ser entre 5 e 80 caracteres")
 	private String nome;
 
