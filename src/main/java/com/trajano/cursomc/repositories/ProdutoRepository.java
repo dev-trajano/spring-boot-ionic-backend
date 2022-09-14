82d62fdc59e0eaf4038ffe762c5151d018d06e55
Joanderson Trajano <trajanojoanderson@gmail.com>
2022-09-13 23:40:50 -0300

    Subtotal e total do Pedido

--- src/main/java/com/trajano/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/trajano/cursomc/repositories/ProdutoRepository.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Repository;
 import com.trajano.cursomc.domain.Produto;
 import org.springframework.transaction.annotation.Transactional;
 
-
 import java.util.List;
 
 @Repository
@@ -19,5 +18,5 @@ public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 
     @Transactional(readOnly=true)
     @Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
-    Page<Produto> findDistinctByNomeContainingAndCategoriasIn(@Param("nome") String nome, @Param("categorias") List<Categoria> categorias, Pageable pageRequest);
+    Page<Produto> search(@Param("nome") String nome,@Param("categorias") List<Categoria> categorias, Pageable pageRequest);
 }
